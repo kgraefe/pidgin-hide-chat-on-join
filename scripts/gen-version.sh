29e6f8910ae2c9bbdecdd820b5b7ad739ad22ff5
Konrad Gräfe <konradgraefe@aol.com>
2017-06-27 18:34:51 +0200

    Mingw: Generate config.h
    This replaces the config.h.mingw file that has been shipped with every
    release with a generation script that generates it from the VERSION and
    configure.ac.in files. This helps changing the version on Windows, e.g.
    for development versions.
    
    This has been tested with Cygwin only.
    
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

---
+++ scripts/gen-version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+test -f VERSION
+VERSION=$(cat VERSION)
+
+if [[ -d .git ]]; then
+	VERSION_GIT=$(git describe --tags --always --dirty)
+
+	# Remove leading 'v'
+	VERSION_GIT=${VERSION_GIT#v}
+
+	if [[ $VERSION_GIT == ${VERSION}* ]]; then
+		VERSION=$VERSION_GIT
+	else
+		echo "$(tput setaf 1)ERROR:$(tput sgr0) Git version '$VERSION_GIT' does not match '$VERSION' in VERSION file!" >&2
+		exit 1
+	fi
+fi
+
+echo $VERSION
