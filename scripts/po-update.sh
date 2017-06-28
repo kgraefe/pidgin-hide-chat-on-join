b590a8b61e3e5f533d079a453bcdf91ceb2d2ea3
Konrad Gräfe <konradgraefe@aol.com>
2017-06-28 16:49:20 +0200

    po-update.sh: Add template name
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- scripts/po-update.sh
+++ scripts/po-update.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
 ls -1 src/*.c > po/POTFILES.in
 
 cd po
-intltool-update -po
+intltool-update --pot --gettext-package=pidgin-hide-chat
 
 for f in *.po; do
 	test -f $f && intltool-update ${f%.po}
